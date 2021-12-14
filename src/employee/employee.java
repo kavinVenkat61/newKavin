b9326e802221c15ec18d0755780bbcebd5261c2b
kavinVenkat61 <kavinmech246@gmail.com>
2021-12-14 23:24:26 +0530

    hi

---
+++ src/employee/employee.java
@@ -0,0 +1,46 @@
+package employee;
+
+public class employee {
+
+	public static void main(String[] args) {
+		// Datatype Variable Name = Value;
+		
+		Byte age = 20;
+		  System.out.println("employee age is:" + age );
+		
+		short atmpin  = 8787;
+		  System.out.println("employee atmpin is:" + atmpin);
+		
+		int id = 98974;
+		  System.out.println("employee id is:" + id);
+		
+		long phone = 9894732765l;
+		  System.out.println("employee phone num is:" + phone);
+		
+		float salary = 98165.75f;
+		  System.out.println("emloyee salary is:" + salary);
+		
+		char grade = 'A';
+		  System.out.println("emloyee grade is:" + grade);
+		
+		String email ="mech246@gmail.com";
+		  System.out.println("employee gmail is:" + email);
+		
+		boolean X = true;
+		  System.out.println(true);
+		
+		float y = 20/3F;
+		  System.out.println(y);
+		
+		double z =20/3d;
+		  System.out.println(z);
+		
+		
+		
+				
+				 
+		
+
+	}
+
+}
